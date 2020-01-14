49cbf50e159b9956c4b6b52aedbee14be7227926
Łukasz <kontakt@lukaszwronski.com>
2020-01-14 15:53:16 +0100

    Styles latest posts, add footer to layout and changes styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,7 +11,14 @@ const TemplateWrapper = ({ children }) => (
     </main>
     <footer className="footer">
       <div className="container">
-        
+        <div class="column">
+          <div>
+            <p>Projekt fotograficzno-dziennikarski autorstwa Eweliny Włoch i Mateusza Dziopy</p>
+          </div>
+          <div>
+            <p>Projekt i wykonanie <a href="https://lukaszwronski.com" title="Łukasz Wroński | Programista WordPress Wrocław">Szablony Wordpress budowane od podstaw</a></p>
+          </div>
+        </div>
       </div>
     </footer>
   </div>
